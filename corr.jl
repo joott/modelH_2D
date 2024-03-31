d75b295153b810a7f5ad0fafbd29d4df7a1ed88b
Josh Ott <jkott@quark.physics.ncsu.edu>
2024-03-31 13:27:14 -0400

    updated corr script

--- corr.jl
+++ corr.jl
@@ -1,13 +1,15 @@
-cd(@__DIR__)
+cd("/home/jkott/perm/modelH_2D/dynamics")
 
 using DelimitedFiles
 using JLD2
 using CodecZlib
+using LsqFit
 
 const N = 64
 const mass = ARGS[1]
+const Δt = 0.04
 
-function autocor_loc_2(x, beg, max, n=2)
+function autocor_loc_2(x, t, beg, max, n=2)
 	C = zeros(Complex{Float64},max+1)
 	N = zeros(Int64,max+1)
 	Threads.@threads for tau in 0:max
@@ -17,10 +19,10 @@ function autocor_loc_2(x, beg, max, n=2)
 			@inbounds N[tau+1] = N[tau+1] + 1
 		end
 	end
-	(collect(0:max),  C ./ N)
+    (t[1:max+1] * Δt,  real.(C) ./ (N * real(C[1])))
 end
 
 df=readdlm("output_H_phi_L_$(N)_eta_0.1_mass_$(mass).dat",' ')
-(t, C) = autocor_loc_2((df[:,2].+df[:,3].*1.0im), 1, N^3, 1)
+(t, C) = autocor_loc_2((df[:,2].+df[:,3].*1.0im), df[:,1], 1, N^3, 1)
 
-jldsave("corr_phi_L_$(N)_mass_$(mass).dat", true; t, C)
+jldsave("corr_phi_L_$(N)_mass_$(mass).jld2", true; t, C, mass)
